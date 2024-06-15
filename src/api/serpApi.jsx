39dfefc11c058c574dd957cc4a49e8de37f3fbcc
unknown <yerzhanutkelbayev@gmail.com>
2024-06-15 01:43:02 +0100

    revert serpapi changes

--- src/api/serpApi.jsx
+++ src/api/serpApi.jsx
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
+const API_KEY = '280587d31d387104738faab45918eb4cbdbb1be974f9ef38fb662df2af746d47';
+
 export const fetchImages = async (query) => {
   try {
-    const response = await axios.get(`/api/proxy`, {
-      params: { query },
-    });
-    return response.data;
+    const params = {
+      engine: 'google',
+      api_key: API_KEY,
+      q: query,
+      tbm: 'isch',  // tbm parameter for image search
+      location: 'Austin, Texas',
+    };
+  
+    const response = await axios.get('/api/search', { params });
+    return response.data.images_results;
   } catch (error) {
     console.error('Error fetching images from SerpApi:', error);
     throw error;
